fcc7913eb8c8bc61c3d4a548c62050589a5698f2
Christine Contreras <christineec23@yahoo.com>
2021-08-06 10:18:32 -0500

    working

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+def countdown(n)
+    while n >= 1
+        print "#{n} SECOND(S)!\n"
+        n -= 1
+    end 
+    "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(n)
+    while n >= 1
+        puts "#{n} SECOND(S)!"
+        sleep(1)
+        n -= 1
+    end 
+    "HAPPY NEW YEAR!"
 end
